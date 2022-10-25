d6626eb5328dbec95160ae80f20bd00a3f38d0ce
myeongseung <myeongseung8952@gmail.com>
2022-10-25 21:11:00 +0900

    chapter1 - 출력

---
+++ src/main/java/j01_출력/PrintTest1.java
@@ -0,0 +1,32 @@
+package j01_출력;
+
+public class PrintTest1 {
+    public static void main(String[] args) {
+        /**
+         * @main
+         *  클래스 또는 메쏘드(함수)가 어떤 기능을 하는지 메모할때 /**사용
+         */
+        System.out.println("출력");
+
+        // 문자열 출력
+        System.out.print("문자열\n");
+        // " " 큰 따옴표 사이는 문자열을
+        //System.out.println('10');
+        // ''작은 따옴표 사이는 한 문자만
+        // System.out.println('1');
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+        System.out.print("이름:");
+        System.out.println("권명승");
+        System.out.print("주소:");
+        System.out.println("부산 ");
+
+        boolean ISUSERNAMECHECKFLAG = true;
+        int level = 0;
+
+
+    }
+}
